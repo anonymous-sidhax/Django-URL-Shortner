5dfba0df31a44a2580c6c26dab359ac52de6e6d1
anonymous-sidhax <72407218+anonymous-sidhax@users.noreply.github.com>
2020-10-11 03:00:42 +0530

    Added Validation for Signup

--- DUS/mainApp/views.py
+++ DUS/mainApp/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, HttpResponseRedirect, redirect
 from .models import Keys, Shorten_Urls
+from django.contrib.auth.models import User
+from django.contrib import messages
 
 # USED_FOR_MAPPING = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_-"
 
@@ -7,6 +9,31 @@ def home(request):
     return render(request, "index.html")
 
 def signup(request):
+
+    if request.method == "POST":
+        if request.POST['password'] == request.POST['password2']:
+            if request.POST['username'] and request.POST['email'] and request.POST['password']:
+                try:
+                    user = User.objects.get(email = request.POST['email'])
+                    return render(request, 'signup.html', { 'error' : 'User already exists'})
+
+                except User.DoesNotExist:
+                    User.objects.create_user(
+                        username = request.POST['password'],
+                        email = request.POST['email'],
+                        password = request.POST['password']
+                    )
+                    messages.success(request, "Signup Sucessful | Login Here")
+                    return redirect(login)
+
+            else:
+                return render(request, 'signup.html', { 'error' : "Empty Fields"}) 
+        else:
+            return render(request, 'signup.html', { 'error' : "Password don't match"})
+    else:
+        return render(request, 'signup.html')
+
+
     return render(request, "signup.html")
 
 def login(request):
