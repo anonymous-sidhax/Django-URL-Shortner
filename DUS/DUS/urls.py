58281d33e64b66eb12ced66eb416f5e9aa508464
anonymous-sidhax <72407218+anonymous-sidhax@users.noreply.github.com>
2020-10-17 22:08:51 +0530

    Fix for console warning
    Warning: 'URL namespace 'admin' isn't unique.'
    Fixed.

--- DUS/DUS/urls.py
+++ DUS/DUS/urls.py
@@ -17,7 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', include('mainApp.urls')),
     path('accounts/', include("accounts.urls", namespace='accounts')),
 ]
