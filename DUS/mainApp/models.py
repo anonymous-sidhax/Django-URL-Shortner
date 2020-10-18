5e8e1a3fe8d559c88f5f8febeaaef9599ae65560
anonymous-sidhax <72407218+anonymous-sidhax@users.noreply.github.com>
2020-10-19 01:49:01 +0530

    Added Updation, Creation, Expiration Date

--- DUS/mainApp/models.py
+++ DUS/mainApp/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import User
+from datetime import datetime
 
 # Create your models here.
 class Keys(models.Model):
@@ -10,8 +12,13 @@ class Keys(models.Model):
 
 class Shorten_Urls(models.Model):
     id = models.AutoField(primary_key=True)
+    user = models.ForeignKey(User, default=15, on_delete=models.CASCADE)
     original_url = models.CharField(blank=False, max_length=200)
     short_url = models.CharField(blank=False, max_length=10, unique=True)
+    creation_date = models.DateTimeField(auto_now_add=True)
+    updation_date = models.DateTimeField(auto_now=True)
+    expiration_date = models.DateTimeField(auto_now=True)
+    expire_flag = models.BooleanField(default=False)
     visits = models.IntegerField(default=0)
 
     def __str__(self):
