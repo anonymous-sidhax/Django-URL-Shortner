d318c4a884810b037d1e0b11ba6e0905cd010b87
anonymous-sidhax <72407218+anonymous-sidhax@users.noreply.github.com>
2020-10-20 02:25:22 +0530

    user wise visit count
    +cleanup

--- DUS/accounts/urls.py
+++ DUS/accounts/urls.py
@@ -1,9 +1,7 @@
 from django.urls import path, include
 from .views import signup_view, login_view, logout_view
 
-
 app_name = 'accounts'
-
 
 urlpatterns = [
     path('login/', login_view, name="login"),
