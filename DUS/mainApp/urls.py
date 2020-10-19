61900aadd3b35570f0aa280729515116c44a1acd
Rohit Kumar Saini <rohitbhatwara1@gmail.com>
2020-10-19 20:11:33 +0530

    added separate page for shorten

--- DUS/mainApp/urls.py
+++ DUS/mainApp/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
 from django.contrib.auth import views 
-from .views import home, redirection, shorten, dashboard
+from .views import home, redirection, shorten, dashboard, shortening_page
 import accounts
 
 
@@ -12,4 +12,5 @@ urlpatterns = [
     path('short', shorten, name="Shorten Url"),
     path('<str:url>', redirection, name="Redirection To Original Page"),
     path('dashboard/', dashboard, name="DUS Dashboard"),
+    path('shorten', shortening_page, name="Shortening Page"),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
