b9670a1eaed5da1978bc41d25a03acdc8da9beb3
Rohit Kumar Saini <40729749+rockingrohit9639@users.noreply.github.com>
2020-10-21 20:16:01 +0530

    Update urls.py

--- DUS/mainApp/urls.py
+++ DUS/mainApp/urls.py
@@ -10,11 +10,6 @@ urlpatterns = [
     path('short', shorten, name="Shorten Url"),
     path('<str:url>', redirection, name="Redirection To Original Page"),
     path('dashboard/', dashboard, name="DUS Dashboard"),
-<<<<<<< HEAD
-    path('shortening/', shortening_page, name="Shortening Page"),
-    path('contact-us/', contactus, name="contactus"),
     path('about-us/', aboutus, name="aboutus"),
-=======
     path('contactus/', contactus, name="contactus"),
->>>>>>> 815ccf499673bef4e1249c46628fea4a8324718c
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
