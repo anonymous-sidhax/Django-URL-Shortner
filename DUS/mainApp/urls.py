f917e2111252f44c79b4bd5dd4ab07f8ada35b64
Aayushpatil77 <aayushpatil558321@gmail.com>
2020-10-21 11:41:13 +0530

    Added Contact us page and model for it

--- DUS/mainApp/urls.py
+++ DUS/mainApp/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
 from django.contrib.auth import views 
-from .views import home, redirection, shorten, dashboard, shortening_page
+from .views import home, redirection, shorten, dashboard, shortening_page, contactus
 
 
 urlpatterns = [
@@ -11,4 +11,5 @@ urlpatterns = [
     path('<str:url>', redirection, name="Redirection To Original Page"),
     path('dashboard/', dashboard, name="DUS Dashboard"),
     path('shortening/', shortening_page, name="Shortening Page"),
+    path('contactus/', contactus, name="contactus"),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
