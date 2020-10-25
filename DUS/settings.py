498587d060d285a907da661d8a2cfddc26f5b020
Aayushpatil77 <aayushpatil558321@gmail.com>
2020-10-25 23:23:32 +0530

    Testing

--- DUS/settings.py
+++ DUS/settings.py
@@ -45,7 +45,9 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+
     'whitenoise.middleware.WhiteNoiseMiddleware',
+    
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -119,6 +121,8 @@ USE_L10N = True
 USE_TZ = True
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+MEDIA_ROOT = BASE_DIR / 'media'
 
 MEDIA_URL = '/images/'
 
