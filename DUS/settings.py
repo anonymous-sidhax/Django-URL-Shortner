65822b8e9392be51113f3b5866438c2f2d23df30
Aayushpatil77 <aayushpatil558321@gmail.com>
2022-01-04 23:14:10 +0530

    Shifted to postgresql

--- DUS/settings.py
+++ DUS/settings.py
@@ -80,14 +80,24 @@ WSGI_APPLICATION = 'DUS.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
+"""
+DATABASES={
+   'default':{
+      'ENGINE':'django.db.backends.postgresql_psycopg2',
+      'NAME':'postgres',
+      'USER':'postgres',
+      'PASSWORD':'12345',
+      'HOST':'localhost',
+      'PORT':'5432',
+   }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
